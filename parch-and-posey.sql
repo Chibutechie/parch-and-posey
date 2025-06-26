8b85670a931316961d907639ea745058f9d75828
unknown <chibuezeanalyst@gmail.com>
2025-06-26 20:28:07 +0100

    wrote a query to display the order id, account id and the total money spent in all the orders, then I ordered by ammount id asc, and total_amt_usd desc

--- parch-and-posey.sql
+++ parch-and-posey.sql
@@ -16456,3 +16456,11 @@ select id, account_id, total_amt_usd
 from orders
 order by total_amt_usd 
 limit 20;
+
+/* Write a query that displays the order ID, account ID, and total dollar amount for 
+all the orders, sorted first by the account ID (in ascending order), 
+and then by the total dollar amount (in descending order). */
+
+select id, account_id, total_amt_usd
+from orders
+order by account_id, total_amt_usd desc;
