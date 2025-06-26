6d35a27e427f9bfa3cdff9204e38ed683a8029ca
unknown <chibuezeanalyst@gmail.com>
2025-06-26 20:32:15 +0100

    wrote the same query as before, but this time sorted first by total dollar amount desc and then by account id asc

--- parch-and-posey.sql
+++ parch-and-posey.sql
@@ -16464,3 +16464,11 @@ and then by the total dollar amount (in descending order). */
 select id, account_id, total_amt_usd
 from orders
 order by account_id, total_amt_usd desc;
+
+/* Now write a query that again displays order ID, account ID, and total dollar amount
+for each order, but this time sorted first by total dollar amount (in descending order), 
+and then by account ID (in ascending order). */
+
+select id, account_id, total_amt_usd
+from orders
+order by total_amt_usd desc, account_id asc;
