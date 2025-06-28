7dd4f8f91323e5b7956cccf30d96612dbc04330a
unknown <chibuezeanalyst@gmail.com>
2025-06-28 03:54:03 +0100

    wrote a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.

--- parch-and-posey.sql
+++ parch-and-posey.sql
@@ -16661,3 +16661,10 @@ Only include the id field in the resulting table.*/
 select id
 from orders
 where gloss_qty > 4000 or poster_qty > 4000;
+
+/* Write a query that returns a list of orders where the standard_qty is zero and either 
+the gloss_qty or poster_qty is over 1000. */
+
+select *
+from orders
+where standard_qty = 0 and (gloss_qty > 1000 or poster_qty > 1000);
