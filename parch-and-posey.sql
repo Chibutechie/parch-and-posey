1f8976e28ec3ccca90c490b2152e4276dceddf90
unknown <chibuezeanalyst@gmail.com>
2025-06-28 03:52:51 +0100

    Found a list of order ids where either the gloss_qty or poster_qty is greater than 4000.

--- parch-and-posey.sql
+++ parch-and-posey.sql
@@ -16654,3 +16654,10 @@ where channel = 'organic' or channel = 'direct'
 order by occurred_at desc;
 
 --- OR STATEMENT --- 
+
+/* Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. 
+Only include the id field in the resulting table.*/
+
+select id
+from orders
+where gloss_qty > 4000 or poster_qty > 4000;
