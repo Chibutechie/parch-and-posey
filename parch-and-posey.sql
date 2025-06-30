97d240c5ad85a9db6833223942c3144c8bd67a87
unknown <chibuezeanalyst@gmail.com>
2025-06-30 14:13:48 +0100

    Wrote a join query that pulls all the data from accounts and orders table

--- parch-and-posey.sql
+++ parch-and-posey.sql
@@ -16680,3 +16680,28 @@ where (name like 'C%' or name like 'W%')
 
 --- JOIN ---
 
+select *
+from orders
+
+select orders.
+from orders
+join accounts
+on orders.account_id = accounts.id;
+
+/* Try pulling all the data from the accounts table, and all the data from 
+the orders table.*/
+
+select accounts.*, orders.*
+from accounts
+join orders
+on accounts.id = orders.account_id;
+
+/*Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, 
+and the website and the primary_poc from the accounts table.*/
+
+select orders.standard_qty, orders.gloss_qty,
+	orders.poster_qty, accounts.website, 
+	accounts.primary_poc
+from orders
+join accounts
+on orders.account_id = accounts.id;
